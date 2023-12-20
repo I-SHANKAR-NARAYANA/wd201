9a6f9cea11c9422a4f4fc93ace458dd2736761d1
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2023-12-20 11:29:31 +0530

    Added http-server

---
+++ http-server/index.js
@@ -0,0 +1,53 @@
+const http = require("http")
+const fs = require("fs")
+const cmdinput = require('minimist')(process.argv.slice(2));
+
+let homeContent = ""
+let projectContent = ""
+let registrationContent = ""
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  });
+
+fs.readFile("projects.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+  startServer()
+});
+function startServer(){
+http.createServer((request, response) => {
+    let url = request.url
+    response.writeHeader(200, {"Content-Type":"text/html"})
+    switch (url) {
+        case "/projects":
+          response.write(projectContent);
+          response.end();
+          break;
+
+        case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+        
+        
+
+        default:
+          response.write(homeContent);
+          response.end();
+          break;
+      }
+})
+.listen(cmdinput.port)
+}
