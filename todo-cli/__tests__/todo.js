653a788930784499a82205784cd03c22bcc407bd
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2023-12-20 14:38:25 +0530

    Added 3 more tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Todo List Test Suite", () => {
   beforeAll(() => {
@@ -25,5 +26,28 @@ describe("Todo List Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("Should Retrieve Overdue Todos", () => {
+    const overdueTodos = overdue();
+    overdueTodos.forEach((todo) => {
+      expect(todo.dueDate).toBe(yesterday);
+    });
+  });
+
+  test("Should Retrieve DueToday Todos", () => {
+    const dueTodayTodos = dueToday();
+    var dateToday = new Date();
+    const today = dateToday.toISOString().split("T")[0];
+    dueTodayTodos.forEach((todo) => {
+      expect(todo.dueDate).toBe(today);
+    });
+  });
+
+  test("Should Retrieve DueLater Todos", () => {
+    const dueLaterTodos = dueLater();
+    dueLaterTodos.forEach((todo) => {
+      expect(todo.dueDate).toBe(tomorrow);
+    });
   });
 });
