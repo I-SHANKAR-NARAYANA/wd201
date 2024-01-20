160b95927af27d087886cc45e6d4c8de8771ed9f
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2024-01-20 11:33:54 +0530

    level 6

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,7 +1,5 @@
-const port = process.env.PORT || 3001;
+const app = require('./app')
 
-const app = require("./app");
-
-app.listen(port, () => {
-  console.log(`Started express server at port ${port}`);
-});
+app.listen(3001, () => {
+  console.log('Started express server at port 3001')
+})
---
+++ todo-app/migrations/20240119073433-create-todo.js
@@ -0,0 +1,37 @@
+/* eslint-disable eol-last */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable semi */
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY
+      },
+      completed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Todos');
+  }
+};
