72278dc3e46f072fa042fbfa6d6dcd30295f2727
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2024-01-23 17:10:53 +0530

    Lvel 9 try

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -21,43 +21,6 @@ module.exports = (sequelize, DataTypes) => {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
-    static async empty(code) {
-      if (code === 1) {
-        await this.addTodo({
-          title: "Buy yesterday",
-          dueDate: new Date(
-            new Date().setDate(new Date().getDate() - 1)
-          ).toISOString(),
-          completed: false,
-        });
-      } else if (code === 3) {
-        await this.addTodo({
-          title: "Buy later",
-          dueDate: new Date(
-            new Date().setDate(new Date().getDate() + 1)
-          ).toISOString(),
-          completed: false,
-        });
-      } else if (code === 2) {
-        await this.addTodo({
-          title: "Buy today",
-          dueDate: new Date().toISOString(),
-          completed: false,
-        });
-      } else {
-        await this.addTodo({
-          title: "Buy completed tomorrow",
-          dueDate: new Date(
-            new Date().setDate(new Date().getDate() + 1)
-          ).toISOString(),
-          completed: true,
-        });
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-        const obj = await this.findOne({ title: "Buy completed tomorrow" });
-        obj.markAsComplete();
-      }
-    }
-
     static async getOverdueTodos() {
       try {
         const allTodos = await Todo.findAll({
@@ -70,22 +33,7 @@ module.exports = (sequelize, DataTypes) => {
             },
           },
         });
-        if (allTodos.length >= 1) {
-          return allTodos;
-        } else {
-          await this.empty(1);
-          const allTodosAgain = await Todo.findAll({
-            where: {
-              dueDate: {
-                [Op.lt]: new Date(),
-              },
-              completed: {
-                [Op.ne]: true,
-              },
-            },
-          });
-          return allTodosAgain;
-        }
+        return allTodos;
       } catch (error) {
         console.error("Error getting overdue todos:", error);
         throw error;
@@ -94,25 +42,9 @@ module.exports = (sequelize, DataTypes) => {
 
     static async getDueTodayTodos() {
       try {
-        const existingTodos = await Todo.findAll({
-          where: {
-            dueDate: new Date().toISOString(),
-            completed: false,
-          },
-        });
-
-        if (existingTodos.length === 0) {
-          await this.empty(2);
-        }
-
         const allTodos = await Todo.findAll({
           where: {
-            dueDate: {
-              [Op.between]: [
-                new Date(),
-                new Date(new Date().setHours(23, 59, 59, 999)),
-              ],
-            },
+            dueDate: new Date().toISOString(),
             completed: false,
           },
         });
@@ -126,19 +58,6 @@ module.exports = (sequelize, DataTypes) => {
 
     static async getDueLaterTodos() {
       try {
-        const existingTodos = await Todo.findAll({
-          where: {
-            dueDate: {
-              [Op.gt]: new Date(),
-            },
-            completed: false,
-          },
-        });
-
-        if (existingTodos.length === 0) {
-          await this.empty(3);
-        }
-
         const allTodos = await Todo.findAll({
           where: {
             dueDate: {
@@ -162,17 +81,7 @@ module.exports = (sequelize, DataTypes) => {
             },
           },
         });
-        if (allCompletedTodos.length === 0) {
-          this.empty(4);
-        }
-        const allCompletedTodosAgain = await Todo.findAll({
-          where: {
-            completed: {
-              [Op.ne]: false,
-            },
-          },
-        });
-        return allCompletedTodosAgain;
+        return allCompletedTodos;
       } catch (error) {
         console.error("Error getting completed todos:", error);
         throw error;
